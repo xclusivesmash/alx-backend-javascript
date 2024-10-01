57dc5742899df4e7c8f55b593f9f77886ff4ad0f
xclusivesmash <sbumatshiane916@gmail.com>
2024-10-01 20:57:10 +0000

    Fixing eslint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-vars */
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
-
