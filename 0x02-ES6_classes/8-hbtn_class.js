69e6bd11e9392d62119353456858f8f585848b38
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-01 08:23:59 +0000

    Initial commit

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,34 @@
+/* HolbertonClass implementation */
+export default class HolbertonClass {
+  /* HolbertonClass definition */
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
