49a4593b736ab83dd467115fd4e73c1e32d44caf
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-01 05:15:47 +0000

    Updated code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -18,7 +18,7 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string.');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
@@ -29,7 +29,7 @@ export default class HolbertonCourse {
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number.');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
@@ -40,7 +40,7 @@ export default class HolbertonCourse {
 
   set students(value) {
     if (!(value instanceof Array)) {
-      throw new TypeError('Students must be an array of strings.');
+      throw new TypeError('Students must be an array of strings');
     }
     if (!value.every((item) => (typeof item === 'string'))) {
       throw new TypeError('Students must be an array of strings');
