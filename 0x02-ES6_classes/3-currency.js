9115d3f89a419fe8b1bf8afeb9189a3574d7624d
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-01 05:33:52 +0000

    Initial commit

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+/* implementantion of currency class */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
