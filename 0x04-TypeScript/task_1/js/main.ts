66b39190ca55a876a592506fb93a2f4f354bf78a
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-21 21:13:19 +0000

    Initial commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,47 @@
-interface Teacher {
+// TypeScript interfaces.
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [key: string]: any;
+}
+// inherits from Teacher
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// interface for printTeacher function
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+// prints initial and surname of teacher instance.
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+// student class
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface IStudentClassContructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName: string;
+  private _lastName: string;
+  constructor(firstName: string, lastName: string): void {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this._firstName;
+  }
 }
