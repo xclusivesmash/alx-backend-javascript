a87a4368260b2e87276bf1a142b3394802dc7cd1
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-28 22:07:50 +0000

    Initial commit

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,38 @@
+/* import modules: 0-calcul and assert */
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+/* testing procedure */
+describe('calculateNumber', function() {
+  it('whole numbers', function() {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('rounding down', function() {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('rounding down a and b\'s floating point fractional number', function() {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+
+  it('rounding down a\'s floating point fractional number', function() {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('rounding up b\'s floating point fractional numbers', function() {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b\'s floating point fractional numbers', function() {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', function() {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', function() {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+  });
+});
