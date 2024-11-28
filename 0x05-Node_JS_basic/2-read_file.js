2aecc0f37e64143a97b445f37845c76321ace136
xclusivesmash <sbumatshiane916@gmail.com>
2024-11-28 08:34:12 +0000

    Initial commit

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,48 @@
+/* import file system handling module */
+const fs = require('fs');
+
+/**
+ * countStudents - logs the number of students
+ * @filename: CSV file
+ */
+const countStudents = (filename) => {
+  /* check if filename is valid */
+  if (!fs.existsSync(filename)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(filename).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = fs
+    .readFileSync(filename, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
+};
+
+/* exporting the parameter(s) */
+module.exports = countStudents;
