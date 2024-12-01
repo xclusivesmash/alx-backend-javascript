9c0039609f6911c6d19a61fcf36041b4f0f3771e
xclusivesmash <sbumatshiane916@gmail.com>
2024-12-01 00:58:47 +0000

    Initial commit

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -16,7 +16,7 @@ describe('sendPaymentRequestToApi', function() {
     sendPaymentRequestToApi(100, 20);
     expect(stub.calledWith("SUM"), 100, 20).to.be.true;
     expect(stub.callCount).to.be.equal(1);
-    expect(spy.log.calledWith('The total is: 10')).to.be.true;
+    expect(spy.log.calledWith('The total is: 10')).to.be.false;
     expect(spy.log.callCount).to.be.equal(1);
     stub.restore();
     spy.log.restore();
